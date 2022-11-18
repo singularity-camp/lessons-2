1e1a5dd18e3cc14530eaf14589bc9c44b20707b6
Talgat <talgat.s@protonmail.com>
2022-11-18 11:44:07 +0600

    first lesson

---
+++ lesson1/objects.js
@@ -0,0 +1,47 @@
+// objects: key - value;
+// let obj = {};
+// obj["name"] = "Harry Potter";
+// // same
+// // obj.name = "Harry Potter";
+// obj["school name"] = "Hogwarts";
+// // obj.schoolName = "Hogwarts";
+// obj["friends"] = {
+//   Ron: true,
+//   Herminone: true,
+// };
+// obj.isChosen = false;
+// obj.isChosen = true;
+// obj[Symbol("n")] = 23;
+// obj[Symbol("n")] = 43;
+// console.log(obj.name);
+
+// array
+// let arr = [1, true, null];
+// let arr2 = arr;
+// arr2.push(2);
+//
+// console.log(arr);
+// let arr = [1, true, null, 2, 3];
+// console.log(arr.includes(4));
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// // Set
+// let set = new Set();
+// set.add("a");
+// set.add(true);
+// set.add(1);
+// set.add([1, 2]);
+// set.add([1, 2]);
+//
+// console.log(set.has(1));
+
+// Map key - value
+// let map = new Map();
+// map.set("a", "b");
+// map.set(true, "name");
+// map.set([1, 2, 3], "name");
+//
+// console.log(map.get(true));
---
+++ lesson1/primitive.js
@@ -0,0 +1,34 @@
+// // 6 primitive
+// // bool
+// let t = true;
+// let t1 = true;
+// // numbers
+// let n = 2.3;
+// // strings
+// let s = "Hello";
+// let s1 = "World";
+// let s4 = "World";
+// console.log(s1 === s4);
+// let s2 = s + s1;
+// let s3 = `${s} ${s1}!`;
+//
+// console.log(s3);
+//
+// // undefined
+// // null
+// let u = null;
+// console.log(u);
+//
+// // symbols
+// let sym = Symbol("a");
+// let sym2 = sym;
+// let sym1 = Symbol("a");
+// console.log(sym === sym2);
+//
+// // typeof
+// console.log(typeof true);
+
+// let a = 2;
+// let b = a;
+// a = 3;
+// console.log(b);
