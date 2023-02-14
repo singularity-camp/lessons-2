4dc3bddb0dd49e38b4ab2433836f50e6d10ed90b
Talgat <talgat.s@protonmail.com>
2023-02-14 11:34:42 +0600

    add keys to buttons

--- lesson22/calculator/src/components/Numpad/Numpad.tsx
+++ lesson22/calculator/src/components/Numpad/Numpad.tsx
@@ -4,7 +4,7 @@ function Numpad() {
   return (
     <div>
       {numbers.map((value) => (
-        <button>{value}</button>
+        <button key={value}>{value}</button>
       ))}
     </div>
   );
